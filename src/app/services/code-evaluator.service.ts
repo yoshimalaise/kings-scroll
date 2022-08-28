b26dfc79353e9a96db42f61f301baf4e46a1423f
yoshimalaise <yoshimalaise@users.noreply.github.com>
2022-08-28 20:56:48 +0200

    added pyonide eval function

--- src/app/services/code-evaluator.service.ts
+++ src/app/services/code-evaluator.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { loadPyodide, PyodideInterface } from 'pyodide';
 import { PropertyCombination } from '../model/property-combination.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodeEvaluatorService {
+  pyodide: PyodideInterface | undefined = undefined;
 
-  constructor() { }
+  constructor() {
+    this.initializePython();
+  }
+
+  async initializePython() {
+    this.pyodide = await loadPyodide();
+  }
 
   evaluateCode(snippet: string): PropertyCombination {
     // eval feels super scary, but should technically be fine 
@@ -15,5 +23,14 @@ export class CodeEvaluatorService {
     const result = eval(`${snippet}\n const extract = () => { return ( {helmet, shield, sword, cape} );} \n extract();`);
     // ^ add extra function since eval gives back the last result
     return result
+  }
+
+  async evaluatePythonCode(snippet: string) {
+    if (!this.pyodide) {
+      throw Error("Pyodide was not properly initialized!");
+    }
+    const result = this.pyodide.runPython(`${snippet}\n{"helmet": helmet, "shield": shield, "sword": sword, "cape": cape}`);
+    console.log('The result of the python code: ', result);
+    return result;
   }
 }
